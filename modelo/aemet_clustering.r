4d54226842bb70164d812f43b26606f5411e1cd7
Fran-FC <frafolcm@gmail.com>
2022-12-13 22:15:57 +0100

    added tipo_clima column

--- modelo/aemet_clustering.r
+++ modelo/aemet_clustering.r
@@ -10,7 +10,9 @@ dt <- fromJSON("dataset/InfoRecortada.json")
 df=dt %>% select(oscilacionTermica, temMed, rachMax, precMaxMen)
 
 df <- as.data.frame(scale(df))
-df <- na.omit(df)
+df = replace(df,is.na(df),0)
+# df <- na.omit(df)
+# df <- na.predict(x=df, )
 
 # first analysis for clustering
 x = c()
@@ -19,7 +21,7 @@ for(i in 1:11) {
   k2 <- kmeans(df, centers = i, nstart= 1)
   x = append(x, i)
   y = append(y, k2$tot.withinss)
-  silhouette(x = k2$cluster, dist = )
+  # silhouette(x = k2$cluster, dist = )
 }
 p = plot(x, y, type="b", pch=16, col="red", 
          main ="Sum of withinss for each number of clusters")
@@ -30,7 +32,7 @@ fviz_nbclust(x=df,kmeans,method = c("silhouette"))
 
 # clean the data with no String varibles
 #spain have 6 diferents 
-k2 <- kmeans(df, centers = 6, nstart = 25)
+k2 <- kmeans(df, centers = 3, nstart = 25)
 
 # uses PCA to plot the data
 fviz_cluster(k2, df, 
@@ -39,10 +41,6 @@ fviz_cluster(k2, df,
              palette = "jco", 
              ggtheme = theme_classic()) #you can change the color palette and theme to your preferences
 
+df$tipo_clima = k2$cluster
+dt$tipo_clima = k2$cluster
 
-
-# data_plot = data.frame(x,y)
-# # col_names c("clusters", "tot_withinss")
-# a = ggplot(data_plot, aes(x,y))
-# a+geom_curve(x, y)
-#        
