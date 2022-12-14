4254c0e2c54e45d478be5aaff1382cfa18e3d34a
Fran-FC <frafolcm@gmail.com>
2022-12-14 19:14:54 +0100

    all dataset completed

--- src/getAllClimateData.py
+++ src/getAllClimateData.py
@@ -3,46 +3,56 @@ import requests
 import json
 import time 
 
-data = None
-with open("dataset/allStations", "r") as f:
-    data = json.loads(f.read())
 
-year = 2001
-url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/{}-01-01T00%3A00%3A00UTC/fechafin/{}-12-31T00%3A00%3A00UTC/estacion/".format(year, year)
+def is_response_correct(resp):
+    if "</html>" in resp:
+        return False
+    return True
 
-querystring = {
-    "api_key":"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmcmFmb2xjbUBnbWFpbC5jb20iLCJqdGkiOiI5YmEwNGI3Yy1lMDY5LTQxYzMtOTU4OC1iY2Q4ZWVhNzMwZmUiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTY3MDI2OTY0NiwidXNlcklkIjoiOWJhMDRiN2MtZTA2OS00MWMzLTk1ODgtYmNkOGVlYTczMGZlIiwicm9sZSI6IiJ9.HkxTYnW4TR_stjkKIgdNocU7pSRxTHn_r4dmK2T09F8"
-}
+if __name__ == "__main__":
+    data = None
+    with open("dataset/allStations", "r") as f:
+        data = json.loads(f.read())
 
-headers = {
-    'cache-control': "no-cache"
-}
+    year = 2021
+    url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/{}-01-01T00%3A00%3A00UTC/fechafin/{}-12-31T00%3A00%3A00UTC/estacion/".format(year, year)
 
-error_response_text = '{\n  "descripcion" : "No hay datos que satisfagan esos criterios",\n  "estado" : 404\n}'
+    querystring = {
+        "api_key":"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmcmFmb2xjbUBnbWFpbC5jb20iLCJqdGkiOiI5YmEwNGI3Yy1lMDY5LTQxYzMtOTU4OC1iY2Q4ZWVhNzMwZmUiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTY3MDI2OTY0NiwidXNlcklkIjoiOWJhMDRiN2MtZTA2OS00MWMzLTk1ODgtYmNkOGVlYTczMGZlIiwicm9sZSI6IiJ9.HkxTYnW4TR_stjkKIgdNocU7pSRxTHn_r4dmK2T09F8"
+    }
 
-tmed_anterior = 0
-for estacion in data:
-    success = False
-    indicativo = estacion["indicativo"]
-    while(not success):
-        success = True
-        time.sleep(0.5)
-        try:
-            resp = requests.request("GET", url+indicativo, headers=headers, params=querystring)
-            if resp.text == error_response_text: 
-                break
+    headers = {
+        'cache-control': "no-cache"
+    }
 
-            json_resp = json.loads(resp.text)
-            dataUrl = json_resp["datos"]
+    error_response_text = '{\n  "descripcion" : "No hay datos que satisfagan esos criterios",\n  "estado" : 404\n}'
 
-            response = requests.request("GET", dataUrl)
-               
-            with open("dataset/estaciones/{}/{}".format(year, indicativo), "w") as f:
-                f.write(response.text)
+    tmed_anterior = 0
+    for estacion in data:
+        success = False
+        indicativo = estacion["indicativo"]
+        while(not success):
+            success = True
+            time.sleep(0.5)
+            try:
+                resp = requests.request("GET", url+indicativo, headers=headers, params=querystring)
+                if resp.text == error_response_text: 
+                    break
 
-            print("Obtenida información de estacion {} del año {}".format(indicativo, year))                    
-        except Exception as e1:
-            print("No se ha podido obtener la url para obtener los datos de la estacion {}\nExcepcion: {}".format(indicativo, e1))
-            success = False
-            time.sleep(2)
-            continue
+                json_resp = json.loads(resp.text)
+                dataUrl = json_resp["datos"]
+
+                response = requests.request("GET", dataUrl)
+
+                if not is_response_correct(response.text):
+                    raise Exception("Response not correct")
+                
+                with open("dataset/estaciones/{}/{}".format(year, indicativo), "w") as f:
+                    f.write(response.text)
+
+                print("Obtenida información de estacion {} del año {}".format(indicativo, year))                    
+            except Exception as e1:
+                print("No se ha podido obtener la url para obtener los datos de la estacion {}\nExcepcion: {}".format(indicativo, e1))
+                success = False
+                time.sleep(2)
+                continue
--- src/procesAllClimateData.py
+++ src/procesAllClimateData.py
@@ -1,111 +1,62 @@
 #!/usr/bin/python3
-import requests
 import json
-import time 
+import statistics
 
-data_stations = None
-with open("dataset/allStations", "r") as f:
-    data_stations = json.loads(f.read())
+year = 2021
 
-year = 2001
-url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/{}-01-01T00%3A00%3A00UTC/fechafin/{}-12-31T00%3A00%3A00UTC/estacion/".format(year, year)
-
-querystring = {
-    "api_key":"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmcmFmb2xjbUBnbWFpbC5jb20iLCJqdGkiOiI5YmEwNGI3Yy1lMDY5LTQxYzMtOTU4OC1iY2Q4ZWVhNzMwZmUiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTY3MDI2OTY0NiwidXNlcklkIjoiOWJhMDRiN2MtZTA2OS00MWMzLTk1ODgtYmNkOGVlYTczMGZlIiwicm9sZSI6IiJ9.HkxTYnW4TR_stjkKIgdNocU7pSRxTHn_r4dmK2T09F8"
-}
-
-headers = {
-    'cache-control': "no-cache"
-}
-
-error_response_text = '{\n  "descripcion" : "No hay datos que satisfagan esos criterios",\n  "estado" : 404\n}'
-
-climate_file = "dataset/info" + str(year)
-
-data_stations = []
-
-temp_medias = 0
-temp_maxs = []
-temp_mins = []
-prec_tot = 0
-
-tmed_anterior = 0
-for estacion in data_stations:
-    success = False
-    indicativo = estacion["indicativo"]
-    while(not success):
-        success = True
-        time.sleep(0.5)
-        try:
-            resp = requests.request("GET", url+indicativo, headers=headers, params=querystring)
-            if resp.text == error_response_text: 
-                break
-
-            json_resp = json.loads(resp.text)
-            dataUrl = json_resp["datos"]
-
-            # obtener de la respuesta la url que contendrá los datos meteorológicas de dicha estacion
-            response = requests.request("GET", dataUrl)
-            json_resp = json.loads(response.text)
-                
-            for dia in json_resp:
-                if "tmed" not in dia:
-                    temp_medias += tmed_anterior
-                else:
-                    tmed = float(dia["tmed"].replace(",", "."))
-                    temp_medias += tmed
-
-                    tmed_anterior = tmed
-                
-                if "tmax" not in dia:
-                    if len(temp_maxs) == 0:
-                        temp_maxs.append(tmed_anterior)
-                    else:
-                        temp_maxs.append(temp_maxs[len(temp_maxs)-1])
-                else:
-                    temp_maxs.append(float(dia["tmax"].replace(",", ".")))
-
-                if "tmin" not in dia:
-                    if len(temp_mins) == 0:
-                        temp_mins.append(tmed_anterior)
-                    else:
-                        temp_mins.append(temp_mins[len(temp_mins)-1])
-                else:
-                    temp_mins.append(float(dia["tmin"].replace(",", ".")))
-
-                if "prec" not in dia or dia["prec"] == "Ip" or dia["prec"] == "Acum":
-                    prec = 0.0
-                else:
-                    prec = float(dia["prec"].replace(",", "."))
-                prec_tot += prec
-
-            print("Obtenida información de estacion {} del año {}".format(indicativo, year))                    
-        except Exception as e1:
-            print("No se ha podido obtener la url para obtener los datos de la estacion {}\nExcepcion: {}".format(indicativo, e1))
-            success = False
-            time.sleep(2)
+def get_all_values_by_key(key, list):
+    res = []
+    for l in list:
+        if key not in l:
             continue
-    
-    if resp.text != error_response_text: 
-        info_estacion_año = {
-            "indicativo": indicativo,
-            "nombre": estacion["nombre"],
-            "provincia": estacion["provincia"],
-            "latitud": estacion["latitud"],
-            "longitud": estacion["longitud"],
-            "precTot": prec_tot,
-            "tmin": min(temp_mins),
-            "tmax": max(temp_maxs),
-            "tmed": (temp_medias / len(temp_mins))
-        }
-        data_stations.append(info_estacion_año)
+        val = l[key]
+        try:
+            val = float(val.replace(",", "."))
+        except: 
+            val = 0.0
+        res.append(val)
+    return res
 
-        temp_medias = 0
-        temp_maxs = []
-        temp_mins = []
-        prec_tot = 0
 
-with open(climate_file, "w") as fw:
-    fw.write(json.dumps(data_stations))
+if __name__ == "__main__":
+    all_stations = None
+    with open("dataset/allStations", "r") as f:
+        all_stations = json.loads(f.read())
+
+    climate_file = "dataset/{}_curated".format(year)
 
     data_stations = []
+
+    for station in all_stations:
+        indicativo = station["indicativo"]
+
+        try:
+            f = open("dataset/estaciones/{}/{}".format(year, indicativo), "r")  
+            info_station = json.loads(f.read())
+            
+            tmeds = get_all_values_by_key("tmed", info_station)
+            tmins = get_all_values_by_key("tmin", info_station)
+            tmaxs = get_all_values_by_key("tmax", info_station)
+            precs = get_all_values_by_key("prec", info_station)
+
+            info_estacion_año = {
+                "indicativo": indicativo,
+                "nombre": station["nombre"],
+                "provincia": station["provincia"],
+                "latitud": station["latitud"],
+                "longitud": station["longitud"],
+                "precTot": sum(precs),
+                "precSD": statistics.pstdev(precs) if precs!=[] else "NA",
+                "tMinMed": statistics.mean(tmins) if tmins!=[] else "NA",
+                "tMaxMed": statistics.mean(tmaxs)  if tmaxs!=[] else "NA",
+                "tMed": statistics.mean(tmeds) if tmeds!=[] else "NA",
+                "tMedSD": statistics.pstdev(tmeds) if tmeds!=[] else "NA"
+            }
+            data_stations.append(info_estacion_año)
+        except FileNotFoundError as e:
+            print(e)
+            continue
+
+            
+    with open(climate_file, "w") as fw:
+        fw.write(json.dumps(data_stations))
